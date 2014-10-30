2d0285f58b5f0592c6d64bfb2f1407b853d7723c
Tristan Whitaker <tristanrileyw@gmail.com>
2014-10-30 11:02:59 -0700

    blogproject

---
+++ controller/create-db.php
@@ -0,0 +1,33 @@
+<?php
+
+	require_once(__DIR__ . "/../model/database.php");
+
+	$connection = new mysqli($host, $username, $password);
+
+	if($connection->connect_error){//checking wether or not there was an error connecting to the database
+		die("Error: "  . $connection->connect_error);
+
+	}
+
+	$exists = $connection->select_db($database);//access a data base that exists on server
+
+	if (!$exists) {//checking wether or not it connected with datavase
+		$query = $connection->query("CREATE DATABASE $database");
+	
+
+		if ($query) {//runs when database exists
+			echo "Succesfully created database: " . $database;
+		}
+	}
+	else{//runs when database has already been created
+
+		echo "Database already exists.";
+	}
+
+	$query = $connection->query("CREATE TABLE posts ("//creates table to store blog posts
+		. "id int(11) NOT NULL AUTO_INCREMENT,"//up to 11 values in intergers, cant be post without id
+		. "title varchar(225) NOT NULL,"
+		. "post text NOT NULL,"
+		. "PRIMARY KEY (id)");//creats query that creats table called post and evey post within table has id
+
+	$connection->close();
---
+++ home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Blogs</title>
+	</head>
+	<body>
+		<?php
+			require_once(__DIR__ . "/controller/create-db.php");
+		?>
+	</body>
+</html>
+
