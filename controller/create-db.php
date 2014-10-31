c75363ad322695f3251ee35b92f190f2a2022943
Tristan Whitaker <tristanrileyw@gmail.com>
2014-10-31 11:03:22 -0700

    blog

--- controller/create-db.php
+++ controller/create-db.php
@@ -28,6 +28,14 @@
 		. "id int(11) NOT NULL AUTO_INCREMENT,"//up to 11 values in intergers, cant be post without id
 		. "title varchar(225) NOT NULL,"
 		. "post text NOT NULL,"
-		. "PRIMARY KEY (id)");//creats query that creats table called post and evey post within table has id.
+		. "PRIMARY KEY (id))");//creats query that creats table called post and evey post within table has id.
+
+	//check if table was successfully created
+	if ($query) {
+		echo "<p>Succesfully created table: post</p>";
+	}
+	else{
+		echo "<p>$connection->error</p>";
+	}
 
 	$connection->close();
