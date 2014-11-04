e0958094c2e19b825daf5629812e97b8c3cb1e8a
Tristan Whitaker <tristanrileyw@gmail.com>
2014-11-04 11:00:31 -0800

    video 17,18 updates

---
+++ view/navigation.php
@@ -0,0 +1,7 @@
+<?php
+require_once(__DIR__ . "/../model/config.php")//calls config fom model
+?>
+<nav>
+	<ul><a href="<?php echo $path . "post.php"//echos out link ?>">Blog Post Form</a></li> <? //link to blog posts form ?>
+	</ul>
+</nav>
