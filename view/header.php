459fbd0614acc86438f5bef1c4bfd89fdb9a579a
Tristan Whitaker <tristanrileyw@gmail.com>
2014-11-06 11:18:59 -0800

    added media queries

--- view/header.php
+++ view/header.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
-<meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> 
+
 <html>
 	<head>
 	<!-- used for styling makes it so we dont have to renter styling every time -->
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 	<!-- calls stylesheet from css -->
 		<meta charset="UTF-8">
+		<meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> 
 		<title>Blogs</title>
 	</head>
 	<body>
