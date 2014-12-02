35206b7bcbbe3f1450363966485d920d10c91838
Tristan Whitaker <tristanrileyw@gmail.com>
2014-12-02 11:19:27 -0800

    video 37

---
+++ view/register-form.php
@@ -0,0 +1,27 @@
+
+<form> 
+<!-- sending information -->
+	<div>
+		<label for="email">Email: </label>
+		<input type="text" name="email"/>
+		<!-- creates text area for email -->
+	</div>
+
+	<div>
+		<label for="username">Username: </label>
+		<input type="text"name="username"/> 
+		<!-- creates text area for username -->
+	</div>
+
+	<div>
+		<label for="password">Password: </label>
+		<input type="password"name="password"/> 
+		<!-- creates text area for password -->
+	</div>
+
+	<div>
+		<button type="submit">submit</button>
+		<!-- craetes submit button -->
+
+	</div>
+</form>
